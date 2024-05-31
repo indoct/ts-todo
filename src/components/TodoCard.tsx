f636ec25a7ab01db1683782bf04549167ddd14b9
syoban <syoban@gmail.com>
2024-05-31 18:48:40 +0100

    edit functionality

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,6 +1,8 @@
 import { Box } from 'grommet';
 import { Todo } from '../interfaces';
 import { Trash, Edit, Checkbox, CheckboxSelected } from 'grommet-icons';
+import { useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 
 interface TodoProps {
   task: Todo;
@@ -9,12 +11,24 @@ interface TodoProps {
 }
 
 const TodoCard = ({ task, tasklist, setTasklist }: TodoProps) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTask, setEditTask] = useState<string>(task.body);
+
   function toggleIsDone(id: number) {
-    setTasklist((prevList) => {
-      return prevList.map((task) => {
-        return id === task.id ? { ...task, isDone: !task.isDone } : task;
+    if (edit) {
+      setEdit(!edit);
+      setTasklist((prevList) => {
+        return prevList.map((task) => {
+          return id === task.id ? { ...task, isDone: !task.isDone } : task;
+        });
       });
-    });
+    } else {
+      setTasklist((prevList) => {
+        return prevList.map((task) => {
+          return id === task.id ? { ...task, isDone: !task.isDone } : task;
+        });
+      });
+    }
   }
 
   function deleteTask(id: number, body: string) {
@@ -26,24 +40,74 @@ const TodoCard = ({ task, tasklist, setTasklist }: TodoProps) => {
     }
   }
 
+  console.log(editTask);
+
+  function handleDiscard() {
+    setEditTask(task.body);
+  }
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTasklist(tasklist.map((todo) => (todo.id === id ? { ...todo, body: editTask } : todo)));
+    setEdit(false);
+  };
+
   return (
-    <Box direction="column" pad="medium" className="todo__card">
-      <Box className="tcbtn__cont">
-        <ul className="tc__buttons">
-          <li className="edit">
-            <Edit />
-          </li>
-          <li className="check" onClick={() => toggleIsDone(task.id)}>
-            {task.isDone ? <CheckboxSelected /> : <Checkbox />}
-          </li>
-          <li className="delete" onClick={() => deleteTask(task.id, task.body)}>
-            <Trash />
-          </li>
-        </ul>
-      </Box>
-      <Box className="tc__body" pad={{ horizontal: 'medium' }}>
-        <p className={task.isDone ? 'st' : ''}>{task.body}</p>
-      </Box>
+    <Box
+      direction="column"
+      pad="small"
+      // pad={{ vertical: 'small', horizontal: 'medium' }}
+      className="todo__card"
+      justify="start"
+      fill="horizontal"
+    >
+      <form onSubmit={(e) => handleEdit(e, task.id)}>
+        <Box className="tcbtn__cont">
+          <ul className="tc__buttons">
+            {edit && <button type="submit">save changes</button>}
+            {edit && <button onClick={() => handleDiscard()}>discard</button>}
+            <li
+              className="edit"
+              onClick={() => {
+                if (!edit && !task.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <Edit />
+            </li>
+            <li>
+              <button
+                className={task.isDone ? 'check icon-btn' : 'check icon-btn btn-enb'}
+                onClick={() => toggleIsDone(task.id)}
+              >
+                {task.isDone ? <CheckboxSelected /> : <Checkbox />}
+              </button>
+            </li>
+            <li>
+              <button
+                className={task.isDone ? 'check icon-btn' : 'check icon-btn btn-enb'}
+                onClick={() => deleteTask(task.id, task.body)}
+              >
+                <Trash />
+              </button>
+            </li>
+          </ul>
+        </Box>
+        <Box className="tc__body" fill>
+          <TextareaAutosize
+            className={task.isDone ? 'st tc__input' : 'tc__input'}
+            value={editTask}
+            onChange={(e) => setEditTask(e.target.value)}
+            readOnly={!edit}
+            onClick={() => {
+              if (!edit && !task.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          />
+        </Box>
+      </form>
     </Box>
   );
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,8 +3,15 @@ import { Grid } from 'grommet';
 import TodoCard from './TodoCard';
 
 const TodoList = ({ tasklist, setTasklist }: ListProps) => {
+  // const columns = {
+  //   small: ['auto'],
+  //   medium: ['auto', 'auto'],
+  //   large: ['auto', 'auto', 'auto'],
+  //   xlarge: ['auto', 'auto', 'auto'],
+  // };
+
   return (
-    <Grid columns="medium" gap="small" pad={{ vertical: 'medium' }}>
+    <Grid fill gap="small" columns="medium" pad={{ vertical: 'medium', horizontal: 'large' }} justify="start">
       {tasklist.map((task) => (
         <TodoCard task={task} tasklist={tasklist} setTasklist={setTasklist} key={task.id} />
       ))}
